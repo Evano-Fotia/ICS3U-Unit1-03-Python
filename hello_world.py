31694ed7d0cab8bd482cd10c151b91b25e5abb02
Ubuntu <ubuntu@ip-172-31-28-26.ec2.internal>
2019-10-28 18:46:43 +0000

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by : Evano Fotia
+# Created on : Oct 2019
+# This pritns hello, world
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
